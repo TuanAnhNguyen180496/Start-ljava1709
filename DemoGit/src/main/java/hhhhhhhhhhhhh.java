42ef67a68db787a3c527fb62fbebecb5c6469ba0
DESKTOP-8FV3DRF\TuanAnh <Tuananhnguyen180496@gmail.com>
2018-01-30 20:16:40 +0700

    sua co

--- DemoGit/src/main/java/hhhhhhhhhhhhh.java
+++ DemoGit/src/main/java/hhhhhhhhhhhhh.java
@@ -1,2 +1,11 @@
 public class hhhhhhhhhhhhh {
+    public static void main(String[] args) {
+        int a=100;
+        if(a%2==0){
+            System.out.println("so chan");
+        }
+        else{
+            System.out.println(" so le");}
+    }
+
 }
